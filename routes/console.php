85b26ccbfc6b747d82f4d2d15b1ecc8b605f5218
RifqiAqil-ops <RifqiAqil-ops@github.com>
2026-07-02 14:09:00 +0700

    chore: konfigurasi scheduler dan finalisasi tahap 5

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('gscrip:sync-weather')->hourly()->withoutOverlapping();
+Schedule::command('gscrip:sync-exchange')->daily()->withoutOverlapping();
+Schedule::command('gscrip:sync-news')->hourly()->withoutOverlapping();
+Schedule::command('gscrip:recalculate-risk')->hourly()->withoutOverlapping();
