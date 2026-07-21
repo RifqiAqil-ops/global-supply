aab2229b866f4540f909be1319f0d14238903cf3
RifqiAqil-ops <RifqiAqil-ops@github.com>
2026-07-21 09:48:46 +0700

    debug: display exception details in 500.blade.php error page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', 'Internal Server Error')
 @section('code', '500')
-@section('message', 'Whoops! Something went wrong on our servers. We are looking into it.')
+@section('message', isset($exception) && $exception ? $exception->getMessage() . ' IN ' . $exception->getFile() . ':' . $exception->getLine() : 'Whoops! Something went wrong on our servers.')
