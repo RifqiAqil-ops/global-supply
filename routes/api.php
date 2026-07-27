5c1829492986e18383e4718d5d6ef4a6ddfddfe7
RifqiAqil-ops <RifqiAqil-ops@github.com>
2026-07-28 01:16:28 +0700

    feat(api): add live endpoints to routes/api.php as fallback

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,9 @@ Route::get('/risk', [ApiController::class, 'risk']);
 Route::get('/ports', [ApiController::class, 'ports']);
 Route::get('/news', [ApiController::class, 'news']);
 Route::get('/currency', [ApiController::class, 'currency']);
+
+Route::get('/live-dashboard-metrics', [\App\Http\Controllers\LiveApiController::class, 'dashboardMetrics']);
+Route::get('/live-weather', [\App\Http\Controllers\LiveApiController::class, 'weather']);
+Route::get('/live-exchange-rates', [\App\Http\Controllers\LiveApiController::class, 'exchangeRates']);
+Route::get('/live-news', [\App\Http\Controllers\LiveApiController::class, 'news']);
+Route::get('/live-country-risk/{code}', [\App\Http\Controllers\LiveApiController::class, 'countryRisk']);
